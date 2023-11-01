b06982330d5dfae73d93ee4fd992ba1b08b899d5
chloe <imherejw@gmail.com>
2023-11-01 11:10:35 +0900

    임시 커밋

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
+import Header from "./components/common/Header"
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+      <Header />
+        <Switch>
+          <Route exact path="/">
+            {/* <Home/> */}
+          </Route>
+          
+          <Route path="/signup">
+
+
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  )
 }
 
 export default App;
