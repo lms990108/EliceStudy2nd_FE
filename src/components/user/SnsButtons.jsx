7e260b70aa167d2cb16549e20fa0760bdcdedd0d
chloe <imherejw@gmail.com>
2023-11-08 22:08:51 +0900

    Fix : reset CDN 추가 및 index.html 파일 내용 변경 커밋

--- src/components/user/SnsButtons.jsx
+++ src/components/user/SnsButtons.jsx
@@ -7,12 +7,29 @@ import googleimg from "../../assets/img/user/googlelogin.png";
 
 
 export default function SnsButtons() {
+
+
   return (
     <>
-      <div className="social-buttons">
-        <button><img className="btnimage"  src={ naverimg } alt=" 네이버로그인" /></button>
-        <button><img className="btnimage" src={ kakaoimg } alt=" 카카오로그인" /></button>
-        <button><img className='btnimage' src={ googleimg } alt=" 구글로그인" /></button>
+      <div className="socialButtons">
+         
+        <div className="btnbox">
+          <button>
+            <img className="btnimage" src={naverimg} alt=" 네이버로그인" />
+            <p className="last-account">마지막으로 로그인한 계정입니다.</p> {/* <- style 확인을 위해 임시로 추가! */}
+          </button>
+        </div>
+        <div className="btnbox">
+          <button>
+            <img className="btnimage" src={kakaoimg} alt=" 카카오로그인" />
+          </button>
+        </div>
+        <div className="btnbox">
+          <button>
+            <img className='btnimage' src={googleimg} alt=" 구글로그인" />
+          </button>
+        </div>
+        
       </div>
     </>
   )
